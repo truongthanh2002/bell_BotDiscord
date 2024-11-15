5750e3a07ff20e060be4354f242eb7389d618df7
truongthanh2002 <87577915+truongthanh2002@users.noreply.github.com>
2024-11-15 16:33:54 +0700

    Add files via upload

--- BELL_DISCORD.py
+++ BELL_DISCORD.py
@@ -13,6 +13,8 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Token and sound file
 
+load_dotenv()
+
 TOKEN = os.getenv('DISCORD_TOKEN')
 BELL_SOUND = "D:/MINI/BOT DISCORD/park-church-bell-tolling-01.wav"
 
